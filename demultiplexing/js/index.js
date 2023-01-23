6e41038d484eb3acdc655e43397d436fc666b860
Arnau <arnaualbert2003@gmail.com>
2023-01-23 10:56:44 +0100

    test function onlynumbers

--- demultiplexing/js/index.js
+++ demultiplexing/js/index.js
@@ -168,3 +168,11 @@ function addForm() {
     if (!charStr.match(/^[0-9]+$/))
       e.preventDefault();
   }
+
+
+  function isNumberKey(evt){
+    var charCode = (evt.which) ? evt.which : evt.keyCode
+    if (charCode > 31 && (charCode != 46 &&(charCode < 48 || charCode > 57)))
+        return false;
+    return true;
+}
