7aace90e571d859aaec952769f2dd405948c07ba
Arnau <arnaualbert2003@gmail.com>
2023-01-25 09:35:56 +0100

    v1.0

--- demultiplexing/js/index.js
+++ demultiplexing/js/index.js
@@ -80,7 +80,7 @@ function sendDemultiplexing() {
     var skipRemovingTmpFilesFrom = document.querySelectorAll("#skip_removing_tmp_files");
     var witDB = document.querySelectorAll("#wit_db");
     var total_obj = total_forms.length;
-
+    
 
 
     // for (let i = 0; i < total_obj; i++) {
@@ -89,12 +89,12 @@ function sendDemultiplexing() {
     // }
 
     for (let i = 0; i < total_obj; i++) {
-        if (fasta0[i].value != "" && fasta1[i].value != "" && output_dir[i].value != "" && refGenomes[i].value != "" && sampleNames[i].value != "") {
+        if (fasta0[i].value != "" && fasta1[i].value != "" && output_dir[i].value != "" && refGenomes[i].value != "" && sampleNames[i].value != "" && !isNaN(numberofthreads[i].value) && !isNaN(readsperchunk[i].value)) {
             if (!isNaN(numberofthreads[i].value) && !isNaN(readsperchunk[i].value)) {
                 var demultiplex_params = new Demultiplex(fasta0[i].value, fasta1[i].value, output_dir[i].value, refGenomes[i].value, sampleNames[i].value, numberofthreads[i].value, readsperchunk[i].value, replacements[i].value, skipRemovingTmpFilesFrom[i].value, witDB[i].value)
                 objects.push(demultiplex_params);
             } else {
-                numberofthreads[i].value = 16;
+                numberofthreads[i].value = "16";
                 readsperchunk[i].value = "";
                 var demultiplex_params = new Demultiplex(fasta0[i].value, fasta1[i].value, output_dir[i].value, refGenomes[i].value, sampleNames[i].value, numberofthreads[i].value, readsperchunk[i].value, replacements[i].value, skipRemovingTmpFilesFrom[i].value, witDB[i].value)
                 objects.push(demultiplex_params);
