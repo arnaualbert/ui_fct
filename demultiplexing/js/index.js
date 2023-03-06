57a7887f74d9186ff7e81b749619b10575e775ba
Arnau <arnaualbert2003@gmail.com>
2023-03-06 08:57:43 +0100

    copy button

--- demultiplexing/js/index.js
+++ demultiplexing/js/index.js
@@ -101,7 +101,14 @@ function addFormReferenceOrganism() {
 //   }
 
 // }
-
+function copiarAlPortapapeles(id_elemento) {
+    var aux = document.createElement("input");
+    aux.setAttribute("value", document.getElementById(id_elemento).innerHTML);
+    document.body.appendChild(aux);
+    aux.select();
+    document.execCommand("copy");
+    document.body.removeChild(aux);
+}
 
 
 
